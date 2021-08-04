1db5c1c586846d143d6d9faee728e7caeb34f906
hossein.moghimi.ce <hossein.moghimi.ce@gmail.com>
2021-08-05 03:26:40 +0430

    context['result']=FAILED

--- projectmanager/apis.py
+++ projectmanager/apis.py
@@ -30,6 +30,7 @@ class ProjectApi(APIView):
 
     def add_project(self,request,*args, **kwargs):
         context={}
+        context['result']=FAILED
         log=1
         if request.method=='POST':
             log+=1
@@ -39,13 +40,15 @@ class ProjectApi(APIView):
                 title=add_project_form.cleaned_data['title']
                 parent_id=add_project_form.cleaned_data['parent_id']
                 project=ProjectRepo(request=request).add_project(parent_id=parent_id,title=title)
-                context['project']=ProjectSerializer(project).data
+                if project is not None:
+                    context['project']=ProjectSerializer(project).data
         context['result']=SUCCEED
         context['log']=log
         return JsonResponse(context)
     
     def edit_project(self,request,*args, **kwargs):
         context={}
+        context['result']=FAILED
         log=1
         if request.method=='POST':
             log+=1
@@ -64,8 +67,9 @@ class ProjectApi(APIView):
                 start_date=PersianCalendar().to_gregorian(start_date)
                 end_date=PersianCalendar().to_gregorian(end_date)
                 project=ProjectRepo(request=request).edit_project(contractor_id=contractor_id,employer_id=employer_id,project_id=project_id,percentage_completed=percentage_completed,start_date=start_date,end_date=end_date,status=status)
-                context['project']=ProjectSerializer(project).data
-        context['result']=SUCCEED
+                if project is not None: 
+                    context['project']=ProjectSerializer(project).data
+                    context['result']=SUCCEED
         context['log']=log
         return JsonResponse(context)
     def add_signature(self,request,*args, **kwargs):
@@ -93,6 +97,7 @@ class ProjectApi(APIView):
 class EventApi(APIView):
     def add_event(self,request,*args, **kwargs):
         context={}
+        context['result']=FAILED
         log=1
         if request.method=='POST':
             log+=1
@@ -103,14 +108,16 @@ class EventApi(APIView):
                 event_datetime=add_event_form.cleaned_data['event_datetime']
                 project_id=add_event_form.cleaned_data['project_id']
                 event=EventRepo(request=request).add_event(event_datetime=event_datetime,project_id=project_id,title=title)
-                context['event']=EventSerializer(event).data
-        context['result']=SUCCEED
+                if event is not None:
+                    context['event']=EventSerializer(event).data
+                    context['result']=SUCCEED
         context['log']=log
         return JsonResponse(context)
 
 class OrganizationUnitApi(APIView):
     def add_organization_unit(self,request,*args, **kwargs):
         context={}
+        context['result']=FAILED
         log=1
         if request.method=='POST':
             log+=1
@@ -133,6 +140,7 @@ class OrganizationUnitApi(APIView):
         return JsonResponse(context)
     def add_employer(self,request,*args, **kwargs):
         context={}
+        context['result']=FAILED
         log=1
         if request.method=='POST':
             log+=1
@@ -141,13 +149,15 @@ class OrganizationUnitApi(APIView):
                 log+=1
                 title=add_employer_form.cleaned_data['title']
                 employer=EmployerRepo(request=request).add_employer(title=title)
-                context['employer']=EmployerSerializer(employer).data
-        context['result']=SUCCEED
+                if employer is not None:
+                    context['employer']=EmployerSerializer(employer).data
+                    context['result']=SUCCEED
         context['log']=log
         return JsonResponse(context)
 
     def add_employee(self,request,*args, **kwargs):
         context={}
+        context['result']=FAILED
         log=1
         if request.method=='POST':
             log+=1
@@ -171,6 +181,7 @@ class OrganizationUnitApi(APIView):
 class MaterialApi(APIView):
     def add_material_request(self,request,*args, **kwargs):
         context={}
+        context['result']=FAILED
         log=1
         if request.method=='POST':
             log+=1
@@ -184,13 +195,15 @@ class MaterialApi(APIView):
                 unit_name=add_material_request_form.cleaned_data['unit_name']
                 unit_price=add_material_request_form.cleaned_data['unit_price']
                 material_request=MaterialRepo(request=request).add_material_request(unit_price=unit_price,unit_name=unit_name,quantity=quantity,material_id=material_id,project_id=project_id)
-                context['material_request']=MaterialRequestSerializer(material_request).data
-                context['result']=SUCCEED
+                if material_request is not None:
+                    context['material_request']=MaterialRequestSerializer(material_request).data
+                    context['result']=SUCCEED
         context['log']=log
         return JsonResponse(context)
 
     def add_material(self,request,*args, **kwargs):
         context={}
+        context['result']=FAILED
         log=1
         if request.method=='POST':
             log+=1
@@ -200,8 +213,9 @@ class MaterialApi(APIView):
                 title=add_material_form.cleaned_data['title']
                 parent_id=add_material_form.cleaned_data['parent_id']
                 material=MaterialRepo(request=request).add_material(parent_id=parent_id,title=title)
-                context['material']=MaterialSerializer(material).data
-        context['result']=SUCCEED
+                if material is not None:
+                    context['material']=MaterialSerializer(material).data
+                    context['result']=SUCCEED
         context['log']=log
         return JsonResponse(context)
 
@@ -210,6 +224,7 @@ class MaterialApi(APIView):
 class ServiceApi(APIView):
     def add_service_request(self,request,*args, **kwargs):
         context={}
+        context['result']=FAILED
         log=1
         if request.method=='POST':
             log+=1
@@ -224,13 +239,15 @@ class ServiceApi(APIView):
                 service_request=ServiceRepo(request=request).add_service_request(unit_price=unit_price,unit_name=unit_name,quantity=quantity,service_title=service_title,project_id=project_id)
                 # print(100*"#76553456")
                 # print(service_request.service.title)
-                context['service_request']=ServiceRequestSerializer(service_request).data
-                context['result']=SUCCEED
+                if service_request is not None:
+                    context['service_request']=ServiceRequestSerializer(service_request).data
+                    context['result']=SUCCEED
         context['log']=log
         return JsonResponse(context)
 
     def add_service(self,request,*args, **kwargs):
         context={}
+        context['result']=FAILED
         log=1
         if request.method=='POST':
             log+=1
@@ -240,8 +257,9 @@ class ServiceApi(APIView):
                 title=add_service_form.cleaned_data['title']
                 parent_id=add_service_form.cleaned_data['parent_id']
                 service=ServiceRepo(request=request).add_service(title=title,parent_id=parent_id)
-                context['service']=ServiceSerializer(service).data
-        context['result']=SUCCEED
+                if service is not None:
+                    context['service']=ServiceSerializer(service).data
+                    context['result']=SUCCEED
         context['log']=log
         return JsonResponse(context)
 
