9b5ea8da4328a3bbd72f4842d3cf60af2956d1f3
hossein.moghimi.ce <hossein.moghimi.ce@gmail.com>
2021-10-01 23:05:52 +0330

    ...

--- phoenix/urls.py
+++ phoenix/urls.py
@@ -10,8 +10,8 @@ from django.conf.urls import url
 # from authentication.views import AuthenticationView
 
 urlpatterns = [
-    path('phoenix4/', include('market.urls')),#for bad redirect
-    path('market/', include('market.urls')),
+    # path('phoenix4/', include('market.urls')),#for bad redirect
+    # path('market/', include('market.urls')),
     path('bms/', include('bms.urls')),
     path('school/', include('school.urls')),
     path('vehicles/', include('vehicles.urls')),
