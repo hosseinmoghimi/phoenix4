21e38fc2d2f5e00b0c8be7c840d64280f87d5be5
hossein.moghimi.ce <hossein.moghimi.ce@gmail.com>
2021-12-10 11:24:36 +0330

    login required for messenger app

--- messenger/urls.py
+++ messenger/urls.py
@@ -4,13 +4,13 @@ from .apps import APP_NAME
 from django.contrib.auth.decorators import login_required
 app_name=APP_NAME
 urlpatterns = [
-    path("",views.BasicViews().home,name="home"),
-    path("message/<int:pk>/",views.MessageViews().message,name="message"),
+    path("",login_required(views.BasicViews().home),name="home"),
+    path("message/<int:pk>/",login_required(views.MessageViews().message),name="message"),
     path("channel/<int:pk>/",login_required(views.ChannelViews().channel),name="channel"),
     # path("event/<int:pk>/",views.EventViews().event,name="event"),
-    path("member/<int:pk>/",views.ChannelViews().member,name="member"),
+    path("member/<int:pk>/",login_required(views.ChannelViews().member),name="member"),
 
     
-    path("send_message/",apis.ChannelApi().send_message,name="send_message"),
-    path("send_notification/",apis.ChannelApi().send_notification,name="send_notification"),
+    path("send_message/",login_required(apis.ChannelApi().send_message),name="send_message"),
+    path("send_notification/",login_required(apis.ChannelApi().send_notification),name="send_notification"),
 ]
