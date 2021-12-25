ead777e9ba89a86ddcb2f96124426ea26a77266b
hossein.moghimi.ce <hossein.moghimi.ce@gmail.com>
2021-12-25 21:05:57 +0330

    ADD proprty image OK

--- realestate/urls.py
+++ realestate/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('agent/<int:pk>/',views.BasicViews().agent,name='agent'),
 
     path('add_location/',apis.PropertyApi().add_location,name="add_location"),
+    path('add_property_image/',apis.PropertyApi().add_property_image,name="add_property_image"),
     path('add_feature/',apis.PropertyApi().add_feature,name="add_feature"),
 ]
