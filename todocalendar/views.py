d6d6586967bbdb98cd349e082233197b7ea5b70c
hossein.moghimi.ce <hossein.moghimi.ce@gmail.com>
2022-02-03 00:19:38 +0330

    add person to calendar app

--- todocalendar/views.py
+++ todocalendar/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-from django.utils.translation import gettext
+
+from authentication.repo import ProfileRepo
 from .repo import AppointmentRepo
 from .apps import APP_NAME
 from django.views import View
-from core.views import CoreContext
+from core.views import CoreContext,PageContext
 
-from todocalendar import apps
+from .forms import *
 
 TEMPLATE_ROOT=APP_NAME+"/"
 layout_parent='phoenix/layout.html'
@@ -25,4 +26,10 @@ class AppointmentViews(View):
         context=getContext(request=request)
         appointment=AppointmentRepo(request=request).appointment(*args, **kwargs)
         context['appointment']=appointment
+        context.update(PageContext(request=request,page=appointment))
+
+
+        context['add_person_form']=AddPersonForm()
+        profiles=ProfileRepo(request=request).list()
+        context['profiles']=profiles
         return render(request,TEMPLATE_ROOT+"appointment.html",context)
