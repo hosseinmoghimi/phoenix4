f18bb26050fd902fcead0dd7627b600352c00dae
hossein.moghimi.ce <hossein.moghimi.ce@gmail.com>
2022-02-05 03:20:07 +0330

    hesabyar zero migration

--- hesabyar/urls.py
+++ hesabyar/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path
 from .apps import APP_NAME
+from django.contrib.auth.decorators import login_required
 from . import views,apis
 app_name=APP_NAME
 urlpatterns = [
-    path('',views.BasicViews().home,name="home"),
-    path('profile_financial_account/<pk>/',views.ReportViews().profile_financial_account,name="profile_financial_account"),
-    path('financial_account/<pk>/',views.ReportViews().financial_account,name="financial_account"),
-    path('financial_document/<pk>/',views.ReportViews().financial_document,name="financialdocument"),
-    path('tag/<pk>/',views.BasicViews().tag,name="tag"),
-    path('add_financial_document/',apis.BasicApi().add_financial_document,name="add_financial_document"),
-    path('financial_document_category/<pk>/',views.ReportViews().financial_document_category,name="financial_document_category"),
+    path('',login_required(views.BasicViews().home),name="home"),
+    path('profile_financial_account/<pk>/',login_required(views.ReportViews().profile_financial_account),name="profile_financial_account"),
+    path('financial_account/<pk>/',login_required(views.ReportViews().financial_account),name="financial_account"),
+    path('financial_document/<pk>/',login_required(views.ReportViews().financial_document),name="financialdocument"),
+    path('tag/<pk>/',login_required(views.BasicViews().tag),name="tag"),
+    path('add_financial_document/',login_required(apis.BasicApi().add_financial_document),name="add_financial_document"),
+    path('financial_document_category/<pk>/',login_required(views.ReportViews().financial_document_category),name="financial_document_category"),
 
 
 ]
